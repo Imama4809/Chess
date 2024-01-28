2cea0879151744743dd1deac9ff5d9d49acd484d
Imama4809 <89104562+Imama4809@users.noreply.github.com>
2024-01-28 10:23:33 -0500

    Delete Chess/sol.py

--- Chess/sol.py
+++
@@ -1,8 +0,0 @@
-def interchange_number_and_letter(inp): # change letters to numbers or numbers to letters
-    l = "0abcdefgh" #list to be able to interchange values, 0 is put at the beginning to make the interchange cleaner 
-    if type(inp) == str:
-        return l.find(inp)
-    if type(inp) == int:
-        return l[inp]
-    
-
